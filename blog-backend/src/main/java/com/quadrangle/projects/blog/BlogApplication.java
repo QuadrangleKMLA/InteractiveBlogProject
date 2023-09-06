2063441ef103b3b46d1602496edb731732b9c149
jaykim0413 <jaylego101@gmail.com>
2023-09-06 23:50:14 +0900

    Updated Blog Application With Command Line Runner

--- blog-backend/src/main/java/com/quadrangle/projects/blog/BlogApplication.java
+++ blog-backend/src/main/java/com/quadrangle/projects/blog/BlogApplication.java
@@ -1,7 +1,12 @@
 package com.quadrangle.projects.blog;
 
+import com.quadrangle.projects.blog.entity.auth.ERole;
+import com.quadrangle.projects.blog.entity.auth.Role;
+import com.quadrangle.projects.blog.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -10,4 +15,18 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (!roleRepository.existsByName(ERole.ROLE_ADMIN)) {
+				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			}
+			if (!roleRepository.existsByName(ERole.ROLE_USER)) {
+				roleRepository.save(new Role(ERole.ROLE_USER));
+			}
+			if (!roleRepository.existsByName(ERole.ROLE_MANAGER)) {
+				roleRepository.save(new Role(ERole.ROLE_MANAGER));
+			}
+		};
+	}
 }
